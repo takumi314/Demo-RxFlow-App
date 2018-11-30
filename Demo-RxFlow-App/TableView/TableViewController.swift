53c3768045ba2279d8a7f5809215be29b6a64440
takumi314 <isospin_symmetry@me.com>
2018-12-01 01:16:49 +0900

    [feat] seup TableViewController

---
+++ Demo-RxFlow-App/TableView/TableViewController.swift
@@ -0,0 +1,57 @@
+//
+//  TableViewController.swift
+//  Demo-RxFlow-App
+//
+//  Created by NishiokaKohei on 29/11/2018.
+//  Copyright © 2018 Takumi. All rights reserved.
+//
+
+import UIKit
+import RxCocoa
+import RxSwift
+
+final class TableViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+
+    private let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: nil)
+    private var viewModel: TableViewViewModel!
+    private let cellIdentifier = "Cell"
+    private let disposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        setupViewModel()
+        setupAddButton()
+        setupTableView()
+        setupTableViewBinding()
+    }
+
+    private func setupViewModel() {
+        self.viewModel = TableViewViewModel(addItemTap: addButton.rx.tap.asDriver())
+    }
+
+    private func setupAddButton() {
+        navigationItem.setRightBarButton(addButton, animated: true)
+    }
+
+    private func setupTableView() {
+        //This is necessary since the UITableViewController automatically set his tableview delegate and dataSource to self
+        tableView.delegate = nil
+        tableView.dataSource = nil
+
+        tableView.tableFooterView = UIView() //Prevent empty rows
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+    }
+
+    private func setupTableViewBinding() {
+
+        viewModel.dataSource
+            .bind(to: tableView.rx.items(cellIdentifier: cellIdentifier, cellType: UITableViewCell.self)) {  row, element, cell in
+                cell.textLabel?.text = "\(element) \(row)"
+            }
+            .disposed(by: disposeBag)
+    }
+
+}
---
+++ Demo-RxFlow-App/TableView/TableViewViewModel.swift
@@ -0,0 +1,41 @@
+//
+//  TableViewViewModel.swift
+//  Demo-RxFlow-App
+//
+//  Created by NishiokaKohei on 29/11/2018.
+//  Copyright © 2018 Takumi. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+class TableViewViewModel {
+
+    // MARK: - Private properties
+
+    private let privateDataSource: BehaviorRelay<[String]> = BehaviorRelay(value: [])
+    private let disposeBag = DisposeBag()
+
+
+    // MARK: - Outputs
+
+    public let dataSource: Observable<[String]>
+
+
+    // MARK: - Initializer
+
+    init(addItemTap: Driver<Void>) {
+
+        // Make the output dataSource an Observable of the privateDataSource
+        self.dataSource = privateDataSource.asObservable()
+
+        // Register addButton tap to append a new "Item" to the dataSource on each tap -> onNext
+        addItemTap.drive(onNext: { [unowned self] _ in
+            self.privateDataSource.accept(self.privateDataSource.value + ["Item"])
+        })
+        .disposed(by: disposeBag)
+        
+    }
+
+}
